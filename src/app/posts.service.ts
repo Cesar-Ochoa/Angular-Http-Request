afdc27777476920e2b99789718b2577823674072
Cesar Ochoa <cesar.ochoa@xpo.com>
2019-12-23 16:26:05 -0600

    Commit
    Get, Put, pipes, httpClient, loading, service for httpRequest, Delete, handling error, Subjects for error handling, setting headers, query params, observing different types of responses, changing the response body type, interceptor, manipulating request objects, response interceptors, multiple interceptors

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpEventType
+} from "@angular/common/http";
 import { Post } from "./post.model";
-import { map } from "rxjs/operators";
+import { map, catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class PostsService {
+  error = new Subject<string>();
+
   constructor(private http: HttpClient) {}
 
   createAndStorePost(title: string, content: string) {
@@ -12,17 +20,41 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         "https://angular-http-request-e3e1e.firebaseio.com/posts.json",
-        postData
+        postData,
+        {
+          observe: "response"
+        }
       )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+      .subscribe(
+        responseData => {
+          console.log(responseData);
+        },
+        error => {
+          this.error.next = error.message;
+        }
+      );
   }
 
   fetchPosts() {
+    //For multiple params
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append("print", "pretty");
+    searchParams = searchParams.append("Custom", "key");
+
     return this.http
-      .get<{ [key: string]: Post }>(
-        "https://angular-http-request-e3e1e.firebaseio.com/posts.json"
+      .get(
+        // .get<{ [key: string]: Post }>(
+        "https://angular-http-request-e3e1e.firebaseio.com/posts.json",
+        {
+          headers: new HttpHeaders({
+            "Custom-Header": "Hello",
+            AditionalHeader: "Another header",
+            ThirdHeader: "Third"
+          }),
+          params: searchParams, //Multiple params
+          //params: new HttpParams().set('print', 'pretty') - SINGLE PARAMS
+          responseType: "json"
+        }
       )
       .pipe(
         map(responsedata => {
@@ -33,6 +65,29 @@ export class PostsService {
             }
           }
           return postsArray;
+        }),
+        catchError(errorRes => {
+          // Send to analytics server
+          return throwError(errorRes);
+        })
+      );
+  }
+
+  deletePosts() {
+    return this.http
+      .delete("https://angular-http-request-e3e1e.firebaseio.com/posts.json", {
+        observe: "events",
+        responseType: "text" //text, blob(files),json
+      })
+      .pipe(
+        tap(event => {
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+            // ...
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
         })
       );
   }
